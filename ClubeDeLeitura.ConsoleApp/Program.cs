79366648569bebf04e8f693bd796bd9f8303c68b
buenoIago <iagocedup@gmail.com>
2026-04-15 13:41:35 -0300

    Adicionado menu principal

--- ClubeDeLeitura.ConsoleApp/Program.cs
+++ ClubeDeLeitura.ConsoleApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("Clube da Leitura");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("1 - Gerenciar caixas de revistas");
+    Console.WriteLine("2 - Gerenciar revistas");
+    Console.WriteLine("3 - Gerenciar amigos");
+    Console.WriteLine("4 - Gerenciar empréstimos");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------");
+    Console.Write("> ");
+    string? opcaoMenuPrincipal = Console.ReadLine()?.ToUpper();
+
+    if (opcaoMenuPrincipal == "S")
+    {
+        Console.Clear();
+        break;
+    }
+
+    while (true)
+    {
+        string? opcaoMenuInterno = string.Empty;
+
+        if (opcaoMenuPrincipal == "1")
+        {
+
+        }
+
+        else if (opcaoMenuPrincipal == "2")
+        {
+
+        }
+
+        else if (opcaoMenuPrincipal == "3")
+        {
+
+        }
+
+        else if (opcaoMenuPrincipal == "4")
+        {
+
+        }
+    }
+}
