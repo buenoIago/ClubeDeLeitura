006d5ced382606990fc8875a9ebcf8440e2ba0f9
buenoIago <iagocedup@gmail.com>
2026-04-16 19:14:06 -0300

    Adicionado funcionalidade de edição de Revista

--- ClubeDeLeitura.ConsoleApp/Apresentacao/TelaRevista.cs
+++ ClubeDeLeitura.ConsoleApp/Apresentacao/TelaRevista.cs
@@ -75,6 +75,39 @@ public class TelaRevista
 
     public void Editar()
     {
+        ExibirCabecalho("Edição de caixa");
+
+        VisualizarTodos(deveExibirCabecalho: false);
+
+        Console.WriteLine("---------------------------------");
+
+        string? idSelecionado;
+
+        do
+        {
+            Console.Write("Informe o ID da revista que deseja editar: ");
+            idSelecionado = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(idSelecionado) && idSelecionado.Length == 7)
+                break;
+        } while (true);
+
+        Console.WriteLine("---------------------------------");
+
+        Revista novaRevista = ObterDadosCadastrais();
+
+        bool ConseguiuEditar = repositorioRevista.Editar(idSelecionado, novaRevista);
+
+        if (!ConseguiuEditar)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Não foi possível encontrar o registro requisitado.");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+
+        ExibirMensagem($"O registro \"{idSelecionado}\" foi editado com sucesso!");
 
     }
 
